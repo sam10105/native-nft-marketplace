5547f3a30d1c6b9c8bbfb47783e8c0d715b066ed
sam10105 <samkasyoki@gmail.com>
2022-06-07 15:09:22 +0300

    setup react navigation

---
+++ screens/Details.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'dripsy';
+
+function Details() {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+}
+
+export default Details;
---
+++ screens/Home.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'dripsy';
+
+function Home() {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+}
+
+export default Home;
